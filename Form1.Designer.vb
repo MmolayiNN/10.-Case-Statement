d561c591504954d04fdbcf839666886cf78be8ca
Hohm <Hohm@HOHM>
2024-01-19 13:57:53 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        TextBox2 = New TextBox()
+        ComboBox1 = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(69, 44)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(150, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter the grade letter"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(69, 123)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Results"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(87, 283)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(126, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Compute"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(238, 116)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"A", "A-", "B", "C", "D"})
+        ComboBox1.Location = New Point(238, 41)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Resulta"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim grade As String
+        grade = ComboBox1.Text
+
+        Select Case grade
+            Case "A"
+                TextBox2.Text = "High Distintion"
+
+            Case "A-"
+                TextBox2.Text = "Distinction"
+
+            Case "B"
+                TextBox2.Text = "Credit"
+
+            Case "C"
+                TextBox2.Text = "Pass"
+
+            Case "D"
+                TextBox2.Text = "Fail"
+            Case Else
+                TextBox2.Text = "Fail"
+
+        End Select
+    End Sub
+End Class
